adcf01e6deb2423c6b67fe96dd7c570521f2cf70
辐无真言 <77034104+zzinx58@users.noreply.github.com>
2023-09-01 16:07:05 +0800

    For first Month.
    aa

--- .output/public/_nuxt/_id_.4711c757.js
+++
@@ -1,1 +0,0 @@
-import{_}from"./entry.ebd3bb8a.js";const r={};function c(e,n){return null}const o=_(r,[["render",c]]);export{o as default};
--- .output/server/chunks/client.mjs
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-const prisma$1 = prisma;
-
-export { prisma$1 as p };
-//# sourceMappingURL=client.mjs.map
