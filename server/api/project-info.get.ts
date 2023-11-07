2dd105b02c6f986b55260ed458ae3637ff552f0e
辐无真言 <77034104+zzinx58@users.noreply.github.com>
2023-11-08 00:33:48 +0800

    append: @env_identicator.

---
+++ server/api/project-info.get.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(async (e) => {
+  // const result = $fetch('https://www.baidu.com');
+  // console.log(result); //Promise<pending>
+  const currentEnv = process.env.VITE_PROJECT_ENV;
+  console.log(currentEnv ?? 'FORMAL')
+  return currentEnv ?? 'FORMAL';
+});
